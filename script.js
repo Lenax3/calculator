81cde133bca722d84fc67360cb9e73880d4482d1
Lenax3 <lena.mick@brainworxx.de>
2026-06-18 10:38:53 +0200

    initial commit basic js

---
+++ script.js
@@ -0,0 +1,16 @@
+///////////ADDITION///////////
+function add(a, b) {
+    return a + b;
+}
+///////////SUBTRACTION///////////
+function subtract(a, b) {
+    return a - b;
+}
+///////////MULTIPLICATION///////////
+function multiply(a, b) {
+    return a * b;
+}
+///////////DIVISION///////////
+function divide(a, b) {
+    return a / b;
+}
